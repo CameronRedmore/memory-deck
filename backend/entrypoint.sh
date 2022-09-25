d9ef18e1a7ef449ec6c9c9c73668455d9f8ef73a
Cameron Redmore <credmore99@gmail.com>
2022-09-25 17:23:00 +0100

    Initial code commit for scanmem

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 
-cd /backend
+cd /backend/scanmem
 
-make
+./autogen.sh
+
+./configure && make
+
+mkdir -p /backend/out
+
+cp -r /backend/scanmem/.libs/libscanmem.so.1.0.0 /backend/out
